beb22a0ed84e8b0c278d14259800bab5f64adbd2
xlnl <lamanhnle@gmail.com>
2020-12-01 15:20:39 -0500

    finished deliverable, might move forward with bonus and add more arithmetic operations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    num1: 0,
+    num2: 0,
+    result: 0
+  }
+
+  setNum = (e, num) => {
+    this.setState({ [num]: e.target.value})
+  }
+
+  addNum = (e) => {
+    var num1 = parseInt(this.state.num1)
+    var num2 = parseInt(this.state.num2)
+    this.setState({
+      result: num1 + num2
+    })
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <h1>Add with React!</h1>
+        <div className="add">
+        <input type="number" name="num1" placeholder="Enter your first number" 
+         value={this.state.num1} onChange={ (e) => this.setNum(e, 'num1')}
+        />
+        <span>+</span>
+        <input type="number" name="num2" placeholder="Enter your second number" 
+         value={this.state.num2} onChange={ (e) => this.setNum(e, 'num2')}
+        />
+        <button onClick={this.addNum}>=</button>
+        <h3>Addition results go here!</h3>
+        <h3>{this.state.result}</h3>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
